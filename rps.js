222bc697d341dbdc4de2d10d44216b2deefa44a8
Klement <anselmo.klement.chua@gmail.com>
2024-07-21 19:07:06 +0800

    Added UI

--- rps.js
+++ rps.js
@@ -1,10 +1,21 @@
+const currentRound = document.querySelector("#currentRound");
+currentRound.textContent = "The results of the current round will be displayed here.";
+const results = document.querySelector("#results");
+results.textContent = "SCORE | You: 0 | Computer: 0";
+const btns = document.querySelector("#btns");
+
+let humanScore = 0;
+let computerScore = 0;
+
+btns.addEventListener("click", (e) => {
+    playRound(e.target.id, getComputerChoice());
+    updateScore();
+    })
+
+
 function getComputerChoice() {
     choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random()*3)];
-}
-
-function getHumanChoice() {
-    return prompt("Rock, paper or scissors?").toLowerCase();
 }
 
 function playRound(humanChoice, computerChoice) {
@@ -12,36 +23,22 @@ function playRound(humanChoice, computerChoice) {
     if (humanChoice !== computerChoice) {
         if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
             humanScore++;
-            console.log("You win this round.");
+            // currentRound.textContent = `You win! ${humanChoice.toTitleCase()} beats ${computerChoice}.`;
         } else {
             computerScore ++;
-            console.log("You lose this round.");
+            // currentRound.textContent = `You lose. ${computerChoice.toTitleCase()} beats ${humanChoice}.`;
         }
     } else {
-        console.log("Tie!")
+        currentRound.textContent = "Tie.";
     }
 }
 
-function playGame(rounds) {
-    for (let i = 1; i <= rounds; i++) {
-        console.log(`Round ${i}!`);
-        console.log(`You: [${humanScore}] : Computer: [${computerScore}]`);
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-    determineWinner(humanScore, computerScore)
-}
+function updateScore() {
+    results.textContent = `SCORE | You: ${humanScore} | Computer: ${computerScore}`;
 
-function determineWinner(human, computer) {
-    if (human === computer) {
-        console.log(`Tie at ${humanScore}:${computerScore}!`);
-    } else if (human > computer) {
-        console.log(`You win ${humanScore}:${computerScore}! :D`);
-    } else {
-        console.log(`You lose ${humanScore}:${computerScore}! :(`);
+    if (humanScore === 5) {
+        results.textContent += `. You win!`
+    } else if (computerScore === 5) {
+        results.textContent += `. You lose. :(`
     }
 }
-
-
-let humanScore = 0;
-let computerScore = 0;
-playGame(5)
